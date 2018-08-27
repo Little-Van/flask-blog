8dba05bb45e2a2e7d0d0ccd69f860456381a1a29
Little-Van <little_van@126.com>
2018-08-27 21:23:20 +0800

    flask_moment exercise

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+from datetime import datetime
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
-    return render_template('hello_world.html')
+    return render_template('hello_world.html', current_time=datetime.utcnow())
 
 
 @app.route('/login/<name>')
