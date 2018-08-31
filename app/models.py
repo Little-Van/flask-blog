bf2aadac6bf4a3d304209ba9468dac95328a8e0b
Little-Van <little_van@126.com>
2018-08-31 21:00:22 +0800

    add password-modle&test_case

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Product(db.Model):
@@ -15,7 +16,19 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128))
     pro_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+
+    @property
+    def password(self):
+        raise AttributeError('password os not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User {}>'.format(self.user_name)
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUNJECT_PREFIX = '[Flasky]'
     FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
     @staticmethod
     def init_app(app):
@@ -15,7 +16,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAIL_SERVER = 'smtp.126.com'
     MAIL_PORT = 465
     MAIL_USE_SSL = True
